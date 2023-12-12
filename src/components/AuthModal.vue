ff095ab9d6b54f493c318fd4bbeb9fecb0418436
fotis <fotis@arx.net>
2023-12-12 13:07:27 +0200

    Validate the Sign Up credentials

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -1,28 +1,39 @@
   <script setup>
-  import { ref, defineProps } from 'vue';
+  import { ref, defineProps, reactive } from 'vue';
+  import { useUserStore } from "../stores/users.js"
+  import { storeToRefs } from 'pinia';
   
   const props = defineProps(['isLogin'])
   const visible = ref(false);
+  const userStore = useUserStore()
 
+  const {errorMessage} = storeToRefs(userStore);
+
+
+  const userCredentials = reactive({
+    email: "",
+    password: "",
+    username: "",
+  })
   const showModal = () => {
     visible.value = true;
   };
   
   const handleOk = (e) => {
-    console.log(e);
-    open.value = false;
+    userStore.handleSingup(userCredentials)
   };
 
   const title =  props.isLogin ? 'Login' : 'Sign Up' 
   </script>
 
 <template>
-    <div>
-      <AButton type="primary" @click="showModal">{{ title }}</AButton>
-      <AModal v-model:visible="visible" :title="title" @ok="handleOk">
-        <aInput class="input" v-if="!isLogin" v-model:value="value" placeholder="Username" />
-        <aInput class="input" v-model:value="value" placeholder="Email" />
-        <aInput class="input" v-model:value="value" placeholder="Password" />
+  <div>
+    <AButton type="primary" @click="showModal">{{ title }}</AButton>
+    <AModal v-model:visible="visible" :title="title" @ok="handleOk">
+        <aInput class="input" v-if="!isLogin" v-model:value="userCredentials.username" placeholder="Username" />
+        <aInput class="input" v-model:value="userCredentials.email" placeholder="Email" />
+        <aInput class="input" v-model:value="userCredentials.password" placeholder="Password" />
+        <ATypographyText v-if="errorMessage" type="danger">{{ errorMessage }}</ATypographyText>
       </AModal>
     </div>
   </template>
--- src/stores/users.js
+++ src/stores/users.js
@@ -1,15 +1,38 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('users', () => {
   const user = ref(null)
+  const errorMessage = ref("")
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
   const handleLogin = () => {
 
   }
 
-  const handleSingup = () => {
+  const handleSingup = (credentials) => {
+    const {email, password, username} = credentials
+
+    if(password.length < 6){
+      return errorMessage.value = "Password length is too short"
+    }
     
+    if(username.length < 4){
+      return errorMessage.value = "Username length is too short"
+    }
+
+    if(!validateEmail(email)){
+      return errorMessage.value = "Wrong E-mail"
+    }
+
+    errorMessage.value =""
   }
 
   const handleLogout = () => {
@@ -20,5 +43,5 @@ export const useUserStore = defineStore('users', () => {
     
   }
 
-  return { user, handleLogin, handleSingup,  handleLogout, getUser }
+  return { user, errorMessage, handleLogin, handleSingup,  handleLogout, getUser }
 })
